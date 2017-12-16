cf47ebc0bb64fef0f368064bf3b66d16310f3c74
ravenusmc <mcuddy77@gmail.com>
2017-12-16 15:53:56 -0500

    fixed minor issues

--- public/js/main.js
+++ public/js/main.js
@@ -3,11 +3,12 @@
 
 //Global Variables
 //This is the array that will hold the candidate info object
-let candidate_info_array = [];
+// let candidate_info_array = [];
 
 /* The purpose of this function is to collect the politician data from the FEC API. 
 The function will then return the data that is collected. */
 function gettingPoliticianData() {
+
   let firstName = document.getElementById('firstname').value;
   let lastName = document.getElementById('lastname').value;
 
@@ -20,23 +21,28 @@ function gettingPoliticianData() {
   //parsing the response 
   let response_parsed = JSON.parse(response);
 
+  // let testOne = response_parsed.results["0"].active_through
+  // document.getElementById('testOne').innerHTML = testOne;
+
   return response_parsed
 };
 
 //This function will pull the specific data that I need from the FEC API.
 function sortingPoliticianData(response_parsed){
 
-  console.log(response_parsed);
+  const candidate_info_array = [];
+
   //This for loop will go through the responses pulling the data that I want
   for (let i = 0; i < response_parsed.results.length; i++){
     //Creating a new object that will hold the data
     let candidateInfo = new Object();
     //Specific data that I want from the response
-    candidate_status = response_parsed.results[i].candidate_status;
-    cycles = response_parsed.results[i].cycles;
-    office_full = response_parsed.results[i].office_full;
-    party_full = response_parsed.results[i].party_full;
-    state = response_parsed.results[i].state;
+    let candidate_status = response_parsed.results[i].candidate_status;
+    let cycles = response_parsed.results[i].cycles;
+    let office_full = response_parsed.results[i].office_full;
+    let party_full = response_parsed.results[i].party_full;
+    let state = response_parsed.results[i].state;
+
     //Setting the data to the object
     candidateInfo.candidate_status = candidate_status;
     candidateInfo.cycles = cycles;
@@ -45,19 +51,25 @@ function sortingPoliticianData(response_parsed){
     candidateInfo.state = state; 
 
     //pushing the object into an array of objects
-    candidate_info_array.push(candidate_info);
-    console.log('Test' + ' ' + candidateInfo.candidate_status);
+    candidate_info_array.push(candidateInfo);
   }
-  return candidate_info_array;
 
+  //Returning the array
+  return candidate_info_array;
 }
+
+// function buildPage (candidate_info_array) {
+//   console.log(candidate_info_array);
+// }
 
 //This function is what luanches when the user his the submit button. 
 function mainFunction() {
 
-  response_parsed = gettingPoliticianData();
-  sortingPoliticianData(response_parsed);
-  //console.log(response_parsed);
+  //Calling the functions that will be used in the program
+  let response_parsed = gettingPoliticianData();
+  let candidate_info_array = sortingPoliticianData(response_parsed);
+  console.log(candidate_info_array);
+  // buildPage(candidate_info_array)
+
 
 }
-
